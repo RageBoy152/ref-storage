d110e151db5a0254ad46978a80cd00a3d771b777
RageBoy152 <89703723+RageBoy152@users.noreply.github.com>
2023-04-15 14:15:06 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,335 @@
+const express = require('express')
+const fs = require('fs');
+const cors = require('cors')
+const path = require('path')
+const util = require('util')
+const unlinkFile = util.promisify(fs.unlink)
+const multer  = require('multer')
+//const serverless = require('serverless-http')
+require('dotenv').config()
+
+
+const app = express()//.Router()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static('data'))
+
+
+app.get('/categories',(req,res)=>{
+    fs.readFile("data/categories.json", "utf8", (err, jsonString) => {
+        res.json(jsonString)
+    })
+})
+
+
+//search functions
+function editDistance(s1, s2) {
+    s1 = s1.toLowerCase();
+    s2 = s2.toLowerCase();
+  
+    var costs = new Array();
+    for (let i = 0; i <= s1.length; i++) {
+      var lastValue = i;
+      for (let j = 0; j <= s2.length; j++) {
+        if (i == 0)
+          costs[j] = j;
+        else {
+          if (j > 0) {
+            var newValue = costs[j - 1];
+            if (s1.charAt(i - 1) != s2.charAt(j - 1))
+              newValue = Math.min(Math.min(newValue, lastValue),
+                costs[j]) + 1;
+            costs[j - 1] = lastValue;
+            lastValue = newValue;
+          }
+        }
+      }
+      if (i > 0)
+        costs[s2.length] = lastValue;
+    }
+    return costs[s2.length];
+  }
+  function similarity(s1, s2) {
+    var longer = s1;
+    var shorter = s2;
+    if (s1.length < s2.length) {
+      longer = s2;
+      shorter = s1;
+    }
+    var longerLength = longer.length;
+    if (longerLength == 0) {
+      return 1.0;
+    }
+    return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
+  }
+app.get('/refs',(req,res)=>{
+    refCat = req.query.category
+    searchQuery = req.query.search
+
+    pageNum = req.query.page
+    pageIndexStart = (pageNum-1)*5
+    pageIndexEnd = pageNum*5
+
+    fs.readFile("data/refs.json", "utf8", (err, jsonString) => {
+        jsonObj = JSON.parse(jsonString)
+        queryRefs = []
+        if (searchQuery==null&&refCat!=null) {
+            for (let i=0;i<jsonObj.length;i++) {
+                //category query
+                if (jsonObj[i].category == refCat)
+                    queryRefs.push(jsonObj[i])
+            }
+        }
+        else if (refCat==null&&searchQuery!=null&&searchQuery!=null) {
+            for (let i=0;i<jsonObj.length;i++) {
+            //search query
+                splitTile = jsonObj[i].title.toLowerCase().split(' ')
+                titleSimilars = []
+                for (let x=0;x<splitTile.length;x++) {
+                    if (similarity(splitTile[x],searchQuery)>=0.7)
+                        titleSimilars.push(2.5)
+                    else {
+                        titleSimilars.push(similarity(splitTile[x],searchQuery)+0.1)
+                    }
+                }
+                if (jsonObj[i].description != '')
+                    titleSimilars.push(similarity(jsonObj[i].description.toLowerCase(),searchQuery))
+                
+                if (similarity(jsonObj[i].title,searchQuery)>=0.6)
+                    titleSimilars.push(similarity(jsonObj[i].title.toLowerCase(),searchQuery)+0.7)
+                
+                if (similarity(jsonObj[i].categoryPath.replace('/','').replace('/','').replace('/',''),searchQuery)>=0.3)
+                    titleSimilars.push(similarity(jsonObj[i].categoryPath.toLowerCase(),searchQuery)+2)
+                //gets avg similarity
+                avgTotal = 0
+                for (let y=0;y<titleSimilars.length;y++) {
+                    avgTotal += titleSimilars[y]
+                }
+                avgSimilar = (avgTotal / titleSimilars.length)
+                
+                //add current ref to array if avg similarity is above threshold
+                if (avgSimilar >= 0.625)
+                    queryRefs.push(jsonObj[i])
+            }
+        }
+        if (queryRefs.length > 5) {
+            origQueryRefsLength = queryRefs.length
+            queryRefs = queryRefs.slice(pageIndexStart,pageIndexEnd)
+            
+            //page nav logic
+            if (((origQueryRefsLength-queryRefs.length) % 5) == 0)
+                finalPage = true
+            else 
+                finalPage = false
+        }   else {
+            finalPage = true
+        }
+        
+        
+        res.json({"refs":queryRefs,"finalPage":finalPage})
+    })
+})
+
+app.get('/refs/images',(req,res)=>{
+    refId = req.query.refId
+    fileExtension = 'png'
+
+    //gets desired imgs file extension
+    imgs = fs.readdirSync(__dirname+'/data/refs')
+    for (i=0;i<imgs.length;i++) {
+        if (parseInt(imgs[i].split('.')[0]) == parseInt(refId)) {
+            fileExtension = imgs[i].split('.')[1]
+            break
+        }
+    }
+    res.sendFile(__dirname+`/data/refs/${refId}.${fileExtension}`)
+})
+app.get('/discordUser',async(req,res)=>{
+    const token = process.env.TOKEN
+
+    const fetchUser = async id => {
+        const response = await fetch(`https://discord.com/api/v9/users/${id}`, {
+            headers: {
+            Authorization: `Bot ${token}`,
+            cors: '*'
+            }
+        })
+        if (!response.ok) throw new Error(`Error status code: ${response.status}`)
+        return await response.json()
+    }
+    res.json(await fetchUser(req.query.userId))
+})
+app.get('/authorityCheck',(req,res)=>{
+    authorityFor = req.query.type
+    userId = req.query.userId
+
+    authorisedList = fs.readFileSync(`data/${authorityFor}Ids.json`)
+    for (let i=0;i<authorisedList.length;i++) {
+        if (authorisedList[i] == userId) {
+            res.status('ok').send()
+            break
+        }
+    }
+    res.status('err').send()
+})
+
+function createFileName() {
+    //gets current ref count by counting files in data/refs
+    fileName = 1
+    fileName = fs.readdirSync('data/refs').length+1
+    
+    //sets multer things
+    storage = multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, __dirname+'/data/refs/')
+        },
+        filename: function (req, file, cb) {
+            const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+            cb(null, fileName + path.extname(file.originalname))
+        }
+    })
+    upload = multer({storage: storage, limits: {fileSize:100000000}})    
+}
+
+app.post('/new-ref',(req,res,next)=>{
+    createFileName()
+    //get other form data from query
+    title = req.query.title
+    desc = req.query.desc
+    cat = req.query.cat
+    catPath = req.query.catPath
+    catPath = catPath.replace(/\s/g, "").replace(/>/g,'/')
+    catPath = catPath.toLowerCase()
+    userId = req.query.userId
+
+    upload.single('image')(req, res, function (err) {
+        if (err) {
+            res.json({msg: err.message})
+        } else {
+            const file = req.file
+            // console.log(file)
+            res.json({msg: 'ok'})
+            
+            //append to refs.json
+            refsJsonFileContent = fs.readFileSync('data/refs.json')
+            refsJsonContent = JSON.parse(refsJsonFileContent)
+
+            refsJsonContent.push({
+                "refId": fs.readdirSync('data/refs').length,
+                "category": cat,
+                "categoryPath": catPath,
+                "title": title,
+                "description": desc,
+                "commentCount": 0,
+                "downloadCount": 0,
+                "uploadedBy": userId,
+                "comments": []
+            })
+            console.log(refsJsonContent)
+            refsJsonFileContent = JSON.stringify(refsJsonContent)
+            fs.writeFileSync('data/refs.json',refsJsonFileContent)
+        }
+    })
+})
+
+
+function getCommentId(refs) {
+    highestCommentId = 0
+    for (let a=0;a<refs.length;a++) {
+        if (refs[a].comments.length > 0) {
+            for (let b=0;b<refs[a].comments.length;b++) {
+                if (refs[a].comments[b].replies.length > 0) {
+                    for (let c=0;c<refs[a].comments[b].replies.length;c++) {
+                        if (refs[a].comments[b].replies[c].commentId > highestCommentId)
+                            highestCommentId = refs[a].comments[b].replies[c].commentId
+                    }
+                }   else {
+                    if (refs[a].comments[b].commentId > highestCommentId)
+                        highestCommentId = refs[a].comments[b].commentId
+                }
+            }
+        }
+    }
+    return highestCommentId
+}
+
+//comments
+app.post('/add-comment',(req,res)=>{
+    type = req.query.type
+    toRef = req.query.toRef
+    comment = req.query.comment
+    commenter = req.query.userId
+
+    jsonStr = fs.readFileSync('data/refs.json')
+    jsonObj = JSON.parse(jsonStr)
+
+    if (type=='comment') {
+        for (let i=0;i<jsonObj.length;i++) {
+            if (jsonObj[i].refId == toRef) {
+                //commentId
+                commentId = getCommentId(jsonObj)
+                
+                jsonObj[i].comments.push({
+                    "commentId": commentId,
+                    "commentedBy": commenter,
+                    "comment": comment,
+                    "replies": []
+                })
+                jsonObj[i].commentCount = jsonObj[i].commentCount + 1
+                fs.writeFileSync('data/commentId.txt',commentId)
+                jsonFile = JSON.stringify(jsonObj)
+                fs.writeFileSync('data/refs.json',jsonFile)
+                res.json({'status':'ok'}).send()
+                break
+            }
+        }
+    }   else {
+        for (let i=0;i<jsonObj.length;i++) {
+            for (let x=0;x<jsonObj.comments.length;x++) {
+                if (jsonObj[i].comments[x].commentId == toRef) {
+                    //commentId
+                    commentId = getCommentId(jsonObj)
+                    console.log(commentId)
+
+                    //append comment
+                    jsonObj[i].comments[x].push({
+                        "commentId": commentId,
+                        "commentedBy": commenter,
+                        "comment": comment,
+                        "replies": []
+                    })
+                    jsonObj[i].commentCount = jsonObj[i].commentCount + 1
+                    fs.writeFileSync('data/commentId.txt',commentId)
+                    jsonFile = JSON.stringify(jsonObj)
+                    fs.writeFileSync('data/refs.json',jsonFile)
+                    res.json({'status':'ok'}).send()
+                    break
+                }
+            }
+        }
+    }
+})
+
+app.get('/incDownloadCount',(req,res)=>{
+    ref = req.query.ref
+
+    jsonStr = fs.readFileSync('data/refs.json')
+    jsonObj = JSON.parse(jsonStr)
+    
+    for (let i=0;i<jsonObj.length;i++) {
+        if (jsonObj[i].refId == ref) {
+            jsonObj[i].downloadCount += 1
+            break
+        }
+    }
+
+    jsonFile = JSON.stringify(jsonObj)
+    fs.writeFileSync('data/refs.json',jsonFile)
+})
+app.listen(3001, ()=>{
+    console.log('Listening on port 3001')
+})
+// app.use('/.netlify/functions/api',app)
+// module.exports.handler = serverless(app)
